8a188c5e9311af40887ab7604c6b0fb3fdd4ddb6
xzenterx <xzenterx7755@gmail.com>
2019-03-25 21:49:57 +0200

    Added model 'Tag', and added tags system.

--- blogengine/blog/models.py
+++ blogengine/blog/models.py
@@ -5,6 +5,7 @@ from django.shortcuts import reverse
 class Post(models.Model):
     title = models.CharField(max_length=150, db_index=True)
     slug = models.SlugField(max_length=150, unique=True)
+    tags = models.ManyToManyField('Tag', blank=True, related_name='posts')
     body = models.TextField(blank=True, db_index=True)
     date_pub = models.DateTimeField(auto_now_add=True)
 
@@ -13,3 +14,17 @@ class Post(models.Model):
 
     def __str__(self):
         return '{}'.format(self.title)
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50, unique=True)
+
+
+    def get_absolute_url(self):
+        return reverse('tag_detail_url', kwargs={'slug': self.slug})
+
+
+    def __str__(self):
+        return '{}'.format(self.title)
+
