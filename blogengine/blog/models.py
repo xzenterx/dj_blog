e0cddb06420fe2d606062cc18e0c777915f655d4
xzenterx <xzenterx7755@gmail.com>
2019-03-26 22:01:12 +0200

    Added form create post and tag.

--- blogengine/blog/models.py
+++ blogengine/blog/models.py
@@ -1,16 +1,28 @@
 from django.db import models
 from django.shortcuts import reverse
+from django.utils.text import slugify
+from time import time
+
+
+def gen_slug(s):
+    new_slug = slugify(s, allow_unicode=True)
+    return new_slug + '-' + str(int(time()))
 
 
 class Post(models.Model):
     title = models.CharField(max_length=150, db_index=True)
-    slug = models.SlugField(max_length=150, unique=True)
+    slug = models.SlugField(max_length=150, blank=True, unique=True)
     tags = models.ManyToManyField('Tag', blank=True, related_name='posts')
     body = models.TextField(blank=True, db_index=True)
     date_pub = models.DateTimeField(auto_now_add=True)
 
     def get_absolute_url(self):
         return reverse('post_detail_url', kwargs={'slug': self.slug})
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = gen_slug(self.title)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return '{}'.format(self.title)
@@ -20,11 +32,8 @@ class Tag(models.Model):
     title = models.CharField(max_length=50)
     slug = models.SlugField(max_length=50, unique=True)
 
-
     def get_absolute_url(self):
         return reverse('tag_detail_url', kwargs={'slug': self.slug})
 
-
     def __str__(self):
         return '{}'.format(self.title)
-
--- blogengine/blog/views.py
+++ blogengine/blog/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 from django.views.generic import View
 
 from .models import *
-from .utils import ObjectDetailMixin
+from .utils import *
+from .forms import *
+
 
 def posts_list(request):
     posts = Post.objects.all()
@@ -22,3 +25,13 @@ class PostDetail(ObjectDetailMixin, View):
 class TagDetail(ObjectDetailMixin, View):
     model = Tag
     template = 'blog/tag_detail.html'
+
+
+class TagCreate(ObjectCreateMixin, View):
+    form_model = TagForm
+    template = 'blog/tag_create.html'
+
+
+class PostCreate(ObjectCreateMixin, View):
+    form_model = PostForm
+    template = 'blog/post_create.html'
