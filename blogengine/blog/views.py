d2bbe47e3f28aba23fbee2a434efed2df2316873
xzenterx <xzenterx7755@gmail.com>
2019-03-26 17:21:44 +0200

    Added Classes Based Views and added Mixin.

--- blogengine/blog/views.py
+++ blogengine/blog/views.py
@@ -1,16 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import View
 
-from .models import Post, Tag
-
+from .models import *
+from .utils import ObjectDetailMixin
 
 def posts_list(request):
     posts = Post.objects.all()
     return render(request, 'blog/index.html', context={'posts': posts})
-
-
-def post_detail(request, slug):
-    post = Post.objects.get(slug__iexact=slug)
-    return render(request, 'blog/post_detail.html', context={'post': post})
 
 
 def tags_list(request):
@@ -18,6 +14,11 @@ def tags_list(request):
     return render(request, 'blog/tags_list.html', context={'tags': tags})
 
 
-def tag_detail(request, slug):
-    tag = Tag.objects.get(slug__iexact=slug)
-    return render(request, 'blog/tag_detail.html', context={'tag': tag})
+class PostDetail(ObjectDetailMixin, View):
+    model = Post
+    template = 'blog/post_detail.html'
+
+
+class TagDetail(ObjectDetailMixin, View):
+    model = Tag
+    template = 'blog/tag_detail.html'
