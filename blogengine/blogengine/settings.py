6630efd872ec8514b0744db411fbf09398e8cb39
xzenterx <xzenterx7755@gmail.com>
2019-03-30 20:14:44 +0200

    Added custom css panel (create, delete, edit).

--- blogengine/blogengine/settings.py
+++ blogengine/blogengine/settings.py
@@ -56,7 +56,8 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'templates')
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'blog/templates')
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -121,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
